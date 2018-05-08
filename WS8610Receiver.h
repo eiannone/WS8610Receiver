a28eceac230b27db05ce68a15b34b782d9208f48
Emanuele Iannone <eiannone@users.noreply.github.com>
2018-05-08 22:04:45 +0200

    Documented logic by @Joetgithub

--- WS8610Receiver.h
+++ WS8610Receiver.h
@@ -3,6 +3,58 @@
   Tested with the following transimitter models: TX3-TH, TX4 and TX7U
 
   Based on RCSwitch library, by Suat Özgür (https://github.com/sui77/rc-switch/)
+  
+  OVERVIEW OF MAIN LOGIC 
+  (by @Joetgithub https://github.com/Joetgithub/TX7U/blob/master/TX7UReceiver.ino)
+  
+  Continously loads pulses into a rolling buffer that is sized to hold one temp
+  or humidity reading. About every 57 seconds the TX4 and TX7 sensors send a
+  data transmission consisting of a 44 bit temperature sequence followed by a
+  repeat of that same 44 bit temperature sequence followed by a 44 bit humidity
+  sequence.  A relativly long delay occurs after each temp/humidity 44 bit sequence
+  and that delay is used as the trigger to evaluate the contents of the buffer and
+  obtain the data values from the respective sequence.
+  A pulse is the time in microseconds between changes in the data pin. The pulses
+  have to follow a rule of being a LONG or SHORT followed by FIXED.
+  A TOLERANCE value specifies the allowable range from the hard coded LONG, SHORT
+  and FIXED pulse times. A 1 bit is a SHORT followed by a FIXED.  A 0 bit is a LONG
+  followed by a SHORT.
+  Here is example of a 10101. Note the variations in the SHORT, LONG and FIXED times.
+            SHORT  LONG SHORT  LONG  LONG
+             505   1300  595   1275  1395
+             ┌-┐  ┌---┐  ┌-┐  ┌---┐  ┌-┐
+             |1|  | 0 |  |1|  | 0 |  |1|
+           --┘ └--┘   └--┘ └--┘   └--┘ └-
+  FIXED        980    1030 1105   950
+  Example showing two timings needed for each pulse
+          t2          t4
+           \           \              t2-t1=pulse1  FIXED
+            ┌----┐     ┌----┐         t3-t2=pulse2  LONG or SHORT
+            |    |     |    |     |   t4-t3=pulse3  FIXED
+        ----┘    └-----┘    └-----┘   t5-t4=pulse4  LONG or SHORT
+       /         /          /
+      t1        t3         t5
+  Because two timings are needed for each bit a total of 88 pulses are needed
+  to decode the 44 bits.
+  The pulses are converted into bits and stored in a six byte array as follows:
+  [0]00001010 [1]11101110 [2]11110100 [3]10010000 [4]01001001 [5]1111
+                   |   \      / |  \      |   |       |   |       |
+     00001010    1110  1110111  1  0100  1001 0000   0100 1001   1111
+  bits: 8         4     4+3=7   1    4    4    4       4    4      4
+  key: (1)       (2)     (3)   (4)  (5)  (6)  (7)     (8)  (9)   (10)
+      header    sensor sensor parity 10s  1s  10ths   10s  10th  check
+                 type    ID    bit                                sum
+  key: 1) Start Sequence is always 0x0A
+       2) sensor 0000 = temp; 1110 = humidity
+       3) sensor id
+       4) parity bit
+       5) ones
+       6) tens
+       7) tenths (for temp)
+       8) repeat ones
+       9) repeat tens
+       10) checksum
+ http://www.f6fbb.org/domo/sensors/tx3_th.php  
 */
 
 #ifndef WS8610Receiver_h
